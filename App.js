449d55ca78e6d72c64df484499505955d611f012
Raissa <raissamalves@gmail.com>
2024-10-05 00:48:20 -0300

    feat(p1-pdmn): cria componente de entrada

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-
 import React from 'react'
 import ReactDOM from 'react-dom'
+import TarefaEntrada from './components/TarefaEntrada'
 
 export default class App extends React.Component{
   
@@ -10,7 +10,7 @@ export default class App extends React.Component{
       <div className="container mt-2">
         <div className="row justify-content-center">
           <div className="col-12 col-md-8">
-            <h1 className="d-flex justify-content-center">Hello, tarefas</h1>
+            <TarefaEntrada />
           </div>
         </div>   
       </div>
---
+++ components/TarefaEntrada.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class TarefaEntrada extends Component {
+  render() {
+    return (
+      <div className="justify-content-center align-items-center border rounded p-3">
+        <div className="mb-3">
+          <input type="text" className="form-control text-center p-3" id="entrada-tarefa" placeholder="Digite a descrição de uma nova tarefa" />
+        </div>
+        <div className='mt-1'>
+          <button className='btn btn-primary w-100 p-3'>OK</button>
+        </div>
+      </div>
+    )
+  }
+}
