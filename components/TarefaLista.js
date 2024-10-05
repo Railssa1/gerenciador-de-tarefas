ac118ec69692612be2d44a1ad18673eb2220637e
Melissa Nonato <melinonato@gmail.com>
2024-10-05 15:12:23 -0300

    feat(p1-pdmn): exibe lista ficticia

--- components/TarefaLista.js
+++ components/TarefaLista.js
@@ -5,21 +5,21 @@ export default class TarefaLista extends Component {
       return (
          <div className="justify-content-center align-items-center mt-4 border rounded p-4">
         <div className='row'>
-          <div className='col-xxl-4'>
+          <div className='col-sm-12 col-lg-6 col-xxl-4'>
             <div className='flex-grow-2 border rounded mb-2 ms-4 me-4'>
               <h4 className='text-center'>Preparar aula de programação</h4>
               </div>
             </div>
           </div>
           <div className='row'>
-          <div className='col-xxl-4'>
+          <div className='col-sm-12 col-lg-6 col-xxl-4'>
             <div className='flex-grow-2 border rounded mb-2 ms-4 me-4'>
               <h4 className='text-center'>Fazer feira</h4>
               </div>
             </div>
           </div>
           <div className='row'>
-          <div className='col-xxl-4'>
+          <div className='col-sm-12 col-lg-6 col-xxl-4'>
             <div className='flex-grow-2 border rounded mb-2 ms-4 me-4'>
               <h4 className='text-center'>Preparar marmitas</h4>
               </div>
